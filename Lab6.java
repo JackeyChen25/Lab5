79f772fc7286dd6ccced4b13c8f500313e17c25f
JackeyChen25 <158172341+JackeyChen25@users.noreply.github.com>
2024-02-15 22:33:19 -0500

    Create Lab6.java

---
+++ Lab6.java
@@ -0,0 +1,10 @@
+public class Project6{
+	public static void main(String[] args){
+
+}
+public static int sum (int[] myArray, int myArraySize){
+}
+public static int average (int[] myArray, int myArraySize){
+
+	}
+}
